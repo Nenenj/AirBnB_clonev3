cf2f63a7aa6cfcfd6e7a08a7800c705b1d77533a
Nenenj <nnennanjoku08@gmail.com>
2023-09-14 10:21:00 +0100

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application 
+"""Starts a Flask web application
 that displays "Hello HBNB!" on the root route.
 """
 
@@ -9,10 +9,14 @@ from flask import Flask
 app = Flask(__name__)
 
 # Define a route for the root URL ("/") with strict_slashes=False
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 # Run the application on 0.0.0.0:5000
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
